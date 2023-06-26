184ae0d8f5485599a5fbb772a221fc4e6e1aa36c
pavan-sisodia <114047215+pavan-sisodia@users.noreply.github.com>
2023-06-26 18:11:40 +0530

    aaj hamne Array padha

---
+++ array.js
@@ -0,0 +1,34 @@
+// Arraay
+const supperHeroes = [`Saktiman, Aryaman, Nagraj, dorimon`];
+//console.log(supperHeroes[1])
+
+const number = [1, 2, 3, 4, 5, 6, 7];
+// console.log(number[0]);
+const studentName = ["Ram", "Shyam", "Sheeta", "Babita"];
+// console.log(studentName[0]);
+
+// Array Method 
+studentName.push("Sarita");
+// console.log(studentName);
+studentName.pop()  
+// console.log(studentName);
+studentName.unshift("Hnuman")  
+// console.log(studentName);
+// console.log(studentName.includes("sweta"));
+// console.log(studentName.indexOf("sweta"));
+// console.log(studentName.includes());
+const newNumber = number.join();
+// console.log(number);
+// console.log(newNumber);
+// console.log(typeof newNumber);
+
+
+// slice, splice
+
+console.log("A", number);
+const Nm1 = number.slice(1, 3);
+console.log(Nm1);
+
+console.log("B", number);
+// const Nm2 = number.slice(1, 3);
+// console.log(Nm1);
