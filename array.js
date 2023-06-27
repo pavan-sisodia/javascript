0ce63db8414d1d0b404ec841368d10e9805d3361
pavan-sisodia <114047215+pavan-sisodia@users.noreply.github.com>
2023-06-27 15:01:51 +0530

    complete Array

--- array.js
+++ array.js
@@ -1,5 +1,5 @@
 // Arraay
-const supperHeroes = [`Saktiman, Aryaman, Nagraj, dorimon`];
+// const supperHeroes = [`Saktiman, Aryaman, Nagraj, dorimon`];
 //console.log(supperHeroes[1])
 
 const number = [1, 2, 3, 4, 5, 6, 7];
@@ -25,10 +25,56 @@ const newNumber = number.join();
 
 // slice, splice
 
-console.log("A", number);
+// console.log("A", number);
 const Nm1 = number.slice(1, 3);
-console.log(Nm1);
+// console.log(Nm1);
 
-console.log("B", number);
+// console.log("B", number);
 // const Nm2 = number.slice(1, 3);
 // console.log(Nm1);
+
+// const supper_heroes = ["Saktiman", "Aryaman", "Nagraj", "dorimon"];
+// const marvalHeros = ["Ironman", "Thor", "spiderMan"];
+// supper_heroes.puah[marvalHeros]
+// console.log(supperHeroes)
+
+const Goad = ["Shiv", "Vishnu", "Brahamma"];
+const Tample = ["mahakalesvar", "Omkalesvar", "kedarnath"]
+// Goad.push(Tample);
+// console.log(Goad);
+
+// Goad.concat(Tample);
+// console.log(Goad);
+
+// const AllGoadTample =Goad.concat(Tample)
+// console.log(AllGoadTample); 
+
+const AllGoadAndTample = [...Goad, ...Tample]
+// console.log(AllGoadAndTample);
+
+const NumberOfarray =[1, 2, 3, 4, [2, 3, 4, 5, [3, 4, 5, 6, [4, 5, 6, 7,]]]];
+const NumberOfNewArray = NumberOfarray.flat(Infinity);
+// console.log(NumberOfNewArray);
+
+// console.log(Array.isArray("Pavan"));
+// console.log(Array.from("Pavan"));
+// console.log(Array.from({name:"Pavan"}));
+
+const No1 = 100;
+const No2 = 200;
+const No3 = 300;
+console.log(Array.of(No1,No2,No3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
