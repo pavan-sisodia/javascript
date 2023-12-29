264cb89b705db35df52d0ab5f38deacf277fad57
Pavan <114047215+pavan-sisodia@users.noreply.github.com>
2023-12-29 16:27:46 +0530

    operations

---
+++ Jsr-revision/operation.js
@@ -0,0 +1,19 @@
+// Conversan 
+let score = 33;
+let changeType = String(score)
+// console.log(changeType);
+// console.log(typeof changeType)
+
+// OPERATIONS
+let num1 = 2;
+let num2 = 3;
+let num3 = 4;
+let num4 = num1 + num2 + num3;
+// console.log(num4);
+
+let addNumber = 2 + "2"; //22
+// console.log(addNumber);
+// let number = "2" + 3; //23
+// let number = "2" + 3 +4; // 234
+let number = 2 + 3 + "4"; // 54
+console.log(number);
