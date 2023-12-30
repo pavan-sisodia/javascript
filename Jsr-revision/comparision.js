56eba2b104b69825ff16fd1e24b5bbbaf2d1a0dd
Pavan <114047215+pavan-sisodia@users.noreply.github.com>
2023-12-30 14:22:15 +0530

    comparisionoperator

---
+++ Jsr-revision/comparision.js
@@ -0,0 +1,14 @@
+// Comparision operators
+
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+// console.log(2 != 3);
+
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null == 0);
+// console.log(null === 0);
+
+// console.log(undefined <= 0);
