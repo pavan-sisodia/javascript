3a317b53e42d9192eab1de27cb582fa06473b641
Pavan <114047215+pavan-sisodia@users.noreply.github.com>
2023-12-30 16:12:49 +0530

    detatype

---
+++ Jsr-revision/datatype.js
@@ -0,0 +1,44 @@
+// Detatype
+/*
+Datatype do prkar ke hote hei 
+Premitive or None primitive detatype
+Primitive deatype =>
+Number
+Null
+booliyan
+Bigint
+String
+Symbol
+Undefined
+None Primitive detatype =>
+Array
+Object
+Function
+*/
+
+const herose = ["Spiderman", "Batman", "Ironman"];
+// console.log(typeof herose);
+
+const logedin = {
+    Name: "Pavan",
+    age: 26,
+    Height: "5.75 cm"
+}
+// console.log(logedin);
+
+// const myFunction = function(){
+//     console.log("Hello Thakur");
+// }
+function myFunc(theObject) {
+    theObject.make = "Toyota";
+  }
+  const mycar ={
+    make: "Honda",
+    moddle: "Accord",
+    year: 1998,
+  }
+  console.log(mycar); //mycar
+  myFunc(mycar)
+  console.log(mycar.make); //Toyota
+
+
