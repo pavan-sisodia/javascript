03e0ba6a2703fcdf1a1d3467fae22b5c10c6c6cc
Pavan <114047215+pavan-sisodia@users.noreply.github.com>
2024-01-10 21:44:27 +0530

    starting array practice

---
+++ Jsr-revision/array.js
@@ -0,0 +1,5 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8,]
+// console.log(myArray.concat(1)); // adding a value in last index [1,2,3,4,5,6,7,8,1,]
+const Aray = []
+ Aray.push("Mohan","Rohan", "Abhi")
+console.log(Aray);
