dc786aaebd8b1681831a5af2edef3f88860df4d8
Pavan <114047215+pavan-sisodia@users.noreply.github.com>
2024-01-12 21:20:16 +0530

    practice an Array method

--- Jsr-revision/array.js
+++ Jsr-revision/array.js
@@ -2,4 +2,21 @@ const myArray = [1, 2, 3, 4, 5, 6, 7, 8,]
 // console.log(myArray.concat(1)); // adding a value in last index [1,2,3,4,5,6,7,8,1,]
 const Aray = []
  Aray.push("Mohan","Rohan", "Abhi")
-console.log(Aray);
+// console.log(Aray);
+
+const programingLanguage = ["JavaScript", "Python", "C++", "c"];
+const otherLanguage = ["Go", "Rubby", "Raiks"];
+// const allLanguage = [...programingLanguage, ...otherLanguage] //...here are spried operator
+// console.log(allLanguage);
+
+// console.log(Array.isArray(programingLanguage)); //isArray batata hai ki ye Array hai ya nhi
+// console.log(Array.from("RajdhaniExpress")); //Array.from string ko Array bna  deta hai
+// console.log(Array.of("RajdhaniExpress")); //Array.from string ko Array bna  deta hai
+
+const myNumber = [1, 2, 3, 4, 5];
+// const newNumber = new Set([1,2,3,1,4,3,5])
+// console.log(newNumber);
+
+// console.log();
+
+
